1da41a4df20e662028e19acf5a7cd184518fa415
jeongmu <jeongmupark@gmail.com>
2019-12-23 18:22:45 +0900

    4.2.6 - 목록 뷰 만들기

--- bookmark/views.py
+++ bookmark/views.py
@@ -1,3 +1,9 @@
+from django.views.generic.list import ListView
+from .models import Bookmark
 from django.shortcuts import render
 
 # Create your views here.
+# ListView를 상속해 사용함
+# Bookmark 모델을 임포트 하고 클래스 안에 model = Bookmark라는 구문을 이용해 모델을 설정함.
+class bookmarkListView(ListView):
+    model = Bookmark
